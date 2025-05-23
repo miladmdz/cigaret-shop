472e77391cac4fb3d111aef1c65c2e1d17d4c927
miladmdz <miladmahmoudzadeh5@gmail.com>
2025-05-23 18:53:30 +0330

    feat: add section title component

---
+++ src/components/home/SectionTitle.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SectionTitle = ({ text }) => {
+  return (
+    <div className="flex items-center my-8">
+      <div className="flex-grow h-0.5 bg-gradient-to-r from-primaryOrange-light rounded-full" />
+      <h2 className="mx-4 font-Dana-Medium text-base sm:text-2xl whitespace-nowrap">
+        {text}
+      </h2>
+      <div className="flex-grow h-0.5 bg-gradient-to-l from-primaryOrange-light rounded-full" />
+    </div>
+  );
+};
+
+export default SectionTitle;
