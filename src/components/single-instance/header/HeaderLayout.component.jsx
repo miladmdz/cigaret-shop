976788597acd18a268efb840d6fd5e9f265b2ae1
miladmdz <miladmahmoudzadeh5@gmail.com>
2025-05-23 18:59:53 +0330

    feat: add header layout component

---
+++ src/components/single-instance/header/HeaderLayout.component.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+import HeaderNav from "@/components/single-instance/header/HeaderNav.component";
+import HeaderBanner from "@/components/single-instance/header/HeaderBanner.component";
+import HeaderActionBar from "@/components/single-instance/header/HeaderActionBar.component";
+
+function HeaderLayout() {
+  return (
+    <>
+      <HeaderNav />
+      <HeaderBanner />
+      <HeaderActionBar />
+    </>
+  );
+}
+
+export default HeaderLayout;
