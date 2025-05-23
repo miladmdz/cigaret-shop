d89cdba104988117702ce6346b98f1b5718f48b7
miladmdz <miladmahmoudzadeh5@gmail.com>
2025-05-23 18:51:04 +0330

    feat: add home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,51 @@
 import Image from "next/image";
 
-export default function Home() {
-  return <div></div>;
-}
+import Brands from "@/components/home/Brands.component";
+import MostPopular from "@/components/home/MostPopular.component";
+import Newest from "@/components/home/Newest.component";
+import PromotionalSlider from "@/components/home/PromotionalSlider.component";
+import TransportNotic from "@/components/home/TransportNotic.component";
+
+const Home = () => {
+  const images = ["./images/banner1.jpg", "./images/banner2.webp"];
+
+  return (
+    <div>
+      <PromotionalSlider images={images} />
+
+      <div className="px-4 xl:px-40 pb-5">
+        <MostPopular />
+      </div>
+
+      <div className="relative w-full h-[400px] md:h-[260px] flex flex-col md:flex-row items-center justify-between gap-2 px-4 xl:px-40 py-5 ">
+        <div className="relative w-full md:w-1/2 h-full">
+          <Image
+            className="rounded"
+            src="/images/cigarBanner.webp"
+            alt="cigaret banner"
+            fill
+          />
+        </div>
+
+        <div className="relative w-full md:w-1/2 h-full">
+          <Image
+            className="rounded"
+            src="/images/pipBanner.webp"
+            alt="pip banner"
+            fill
+          />
+        </div>
+      </div>
+
+      <div className="px-4 xl:px-40 pb-5">
+        <Newest />
+      </div>
+
+      <Brands />
+
+      <TransportNotic />
+    </div>
+  );
+};
+
+export default Home;
