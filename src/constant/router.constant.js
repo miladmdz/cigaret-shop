ff504fb95041622e34a95235d9e2b3ee548a4912
miladmdz <miladmahmoudzadeh5@gmail.com>
2025-05-23 19:00:46 +0330

    feat: add constant

---
+++ src/constant/router.constant.js
@@ -0,0 +1,9 @@
+const ROUTER_MAP = [
+  { label: "خانه", to: "/" },
+  { label: "فروشگاه", to: "/shop" },
+  { label: "درباره ما", to: "/aboutus" },
+  { label: "بلاگ", to: "/blogs" },
+  { label: "ارتباط با ما", to: "/contactus" },
+];
+
+export { ROUTER_MAP };
