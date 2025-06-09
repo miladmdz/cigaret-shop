4b2f9502bdb61b1afe3c087d28dd36b993686a8d
miladmdz <miladmahmoudzadeh5@gmail.com>
2025-06-10 00:03:11 +0330

    feat: transfer route blog to articles

--- src/constant/router.constant.js
+++ src/constant/router.constant.js
@@ -2,7 +2,7 @@ const ROUTER_MAP = [
   { label: "خانه", to: "/" },
   { label: "فروشگاه", to: "/product" },
   { label: "درباره ما", to: "/aboutus" },
-  { label: "بلاگ", to: "/blogs" },
+  { label: "مقالات", to: "/articles" },
   { label: "ارتباط با ما", to: "/contactus" },
 ];
 
